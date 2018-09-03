9f59e2fa4f75ccad0ffe8772d09e5cc65c3142e2
Martijn van Dijk <mhvdijk@gmail.com>
2018-09-03 17:25:31 +0200

    Implement plugin

--- android/src/main/kotlin/com/baseflow/googleapiavailability/GoogleApiAvailabilityPlugin.kt
+++ android/src/main/kotlin/com/baseflow/googleapiavailability/GoogleApiAvailabilityPlugin.kt
@@ -5,21 +5,67 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.PluginRegistry.Registrar
+import com.baseflow.googleapiavailability.data.PlayServicesAvailability
+import com.baseflow.googleapiavailability.utils.Codec
 
-class GoogleApiAvailabilityPlugin(): MethodCallHandler {
+class GoogleApiAvailabilityPlugin(val context: Context): MethodCallHandler {
   companion object {
+    val REQUEST_GOOGLE_PLAY_SERVICES = 1000
+
     @JvmStatic
     fun registerWith(registrar: Registrar): Unit {
-      val channel = MethodChannel(registrar.messenger(), "google_api_availability")
-      channel.setMethodCallHandler(GoogleApiAvailabilityPlugin())
+      val channel = MethodChannel(registrar.messenger(), "flutter.baseflow.com/google_api_availability/methods")
+      channel.setMethodCallHandler(GoogleApiAvailabilityPlugin(context = registrar.activity()))
     }
   }
 
   override fun onMethodCall(call: MethodCall, result: Result): Unit {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
-    }
+    when {
+          call.method == "checkPlayServicesAvailability" -> {
+            val googleApiAvailability = GoogleApiAvailability.getInstance()
+            val connectionResult = googleApiAvailability.isGooglePlayServicesAvailable(context)
+            val availability = toPlayServiceAvailability(connectionResult)
+
+            //TODO: Add optional bool to show error dialog
+            //if (GoogleApiAvailability.getInstance().showErrorDialogFragment(context, connectionResult, REQUEST_GOOGLE_PLAY_SERVICES)) {
+            //     return
+            //}
+            result.success(Codec.encodePlayServicesAvailability(availability));
+          }
+          else -> result.notImplemented()
+      }
   }
+
+  private fun toPlayServiceAvailability(connectionResult: int): PlayServicesAvailability {
+        val availability: PlayServicesAvailability
+
+        when (permission) {
+        PlayServicesAvailability.SUCCESS -> {
+            availability = PlayServicesAvailability.SUCCESS
+        }
+        when (permission) {
+        PlayServicesAvailability.SERVICE_MISSING -> {
+            availability = PlayServicesAvailability.SERVICE_MISSING
+        }
+        when (permission) {
+        PlayServicesAvailability.SERVICE_UPDATING -> {
+            availability = PlayServicesAvailability.SERVICE_UPDATING
+        }
+        when (permission) {
+        PlayServicesAvailability.SERVICE_VERSION_UPDATE_REQUIRED -> {
+            availability = PlayServicesAvailability.SERVICE_VERSION_UPDATE_REQUIRED
+        }
+        when (permission) {
+        PlayServicesAvailability.SERVICE_DISABLED -> {
+            availability = PlayServicesAvailability.SERVICE_DISABLED
+        }
+        when (permission) {
+        PlayServicesAvailability.SERVICE_INVALID -> {
+            availability = PlayServicesAvailability.SERVICE_INVALID
+        }
+        else -> availability = PlayServicesAvailability.UNKNOWN
+        }
+
+        return availability;
+    }
 }
--- lib/google_api_availability.dart
+++ lib/google_api_availability.dart
@@ -1,13 +1,44 @@
+library google_api_availability;
+
 import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/services.dart';
+import 'package:meta/meta.dart';
+
+part 'models/google_play_services_availability.dart';
+part 'utils/codec.dart';
 
 class GoogleApiAvailability {
-  static const MethodChannel _channel =
-      const MethodChannel('google_api_availability');
+  factory GoogleApiAvailability() {
+    if (_instance == null) {
+      final MethodChannel methodChannel =
+          const MethodChannel('flutter.baseflow.com/google_api_availability/methods');
+      _instance = new GoogleApiAvailability.private(methodChannel);
+    }
+    return _instance;
+  }
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  @visibleForTesting
+  GoogleApiAvailability.private(this._methodChannel);
+
+  static GoogleApiAvailability _instance;
+
+  final MethodChannel _methodChannel;
+
+  /// This feature is only available on Android devices. On any other platforms
+  /// the [checkPlayServicesAvailability] method will always return
+  /// [GooglePlayServicesAvailability.notAvailableOnPlatform].
+  Future<GooglePlayServicesAvailability>
+      checkGooglePlayServicesAvailability() async {
+    if (!Platform.isAndroid) {
+      return GooglePlayServicesAvailability.notAvailableOnPlatform;
+    }
+
+    final dynamic availability =
+        await _methodChannel.invokeMethod('checkPlayServicesAvailability');
+
+    return Codec.decodePlayServicesAvailability(availability);
   }
 }
